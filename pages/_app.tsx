b3bf51b0364c0da1cf7005777c601fcd0fe3d3c1
Pranav Soni <pranavsoni1101@gmail.com>
2022-04-06 15:01:54 +0530

    Added chakra provider to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import React from "react";
+import { ChakraProvider } from '@chakra-ui/react';
+import { AppProps } from "next/app";
+function App({ Component, pageProps }: AppProps): React.ReactNode {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>  
+    );
 }
-
-export default MyApp
+export default App;
