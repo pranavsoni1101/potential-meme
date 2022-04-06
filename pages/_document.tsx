55af101a8b21e4a040f849ead4c674674b56e31e
Pranav Soni <pranavsoni1101@gmail.com>
2022-04-06 17:40:04 +0530

    Added _document file

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, {
+    Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps
+} from "next/document";
+import { ReactElement } from "react";
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+        const initialProps = await Document.getInitialProps(ctx);
+        return {...initialProps};  
+    }
+    render(): ReactElement {
+        return(
+            <Html lang="en">
+                <Head>
+                    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;300;400;600;700;800;900&display=swap" rel="stylesheet" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument;
