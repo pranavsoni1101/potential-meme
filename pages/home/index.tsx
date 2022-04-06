0927e4a10cf1f6458b2c620f914a5f1075179374
Pranav Soni <pranavsoni1101@gmail.com>
2022-04-06 17:53:30 +0530

    Added home page

---
+++ pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+import { NextPage } from 'next';
+
+const Home: NextPage = () => {
+    return(
+        <Box>
+            Hello From Home
+        </Box>
+    )
+}
+
+export default Home;
