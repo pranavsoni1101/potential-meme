8a75df2a128fd4220fd2c4c7bc5363760bc3d27d
Pranav Soni <pranavsoni1101@gmail.com>
2022-04-06 18:38:33 +0530

    Removed home directory

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,19 @@
-import Home from "./home";
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+import { NextPage } from 'next';
+import Section from '../components/Section';
+import SectionContent from '../components/Section/SectionContent';
 
-const Index = () => <Home />
+const Home: NextPage = () => {
+    return(
+        <>
+            <Section>
+                <SectionContent>
+                    Hello From Home
+                </SectionContent>
+            </Section>
+        </>
+    )
+}
 
-export default Index
+export default Home;
