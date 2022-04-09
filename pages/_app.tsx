bb29ec9d0ba79079e57fea6003019e50c106562c
Pranav Soni <pranavsoni1101@gmail.com>
2022-04-09 23:04:05 +0530

    Added loading component, modified apostophe inhome page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { ReactNode} from "react";
 import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import theme from '../theme/theme';
 
-function App({ Component, pageProps }: AppProps): React.ReactNode {
-  return (
-    <ChakraProvider theme = {theme}>
-      <CSSReset />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>  
-    );
+const App = ({ Component, pageProps }: AppProps): ReactNode => {
+
+    return (
+
+        <ChakraProvider theme={theme}>
+            <CSSReset />
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </ChakraProvider>
+    )
 }
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,117 +6,117 @@ import SectionContent from '../components/Section/SectionContent';
 import Head from 'next/head';
 
 const Home: NextPage = () => {
-    return(
+    return (
         <>
             <Head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com" /> 
-                    <link href="https://fonts.googleapis.com/css2?family=Waiting+for+the+Sunrise&display=swap" rel="stylesheet" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Waiting+for+the+Sunrise&display=swap" rel="stylesheet" />
             </Head>
             <Section>
                 <SectionContent>
                     <Text
-                        color      = "rgba(42,238,234)"
-                        fontSize   = "1.4em"
-                        marginLeft = "-2em"
-                        fontFamily = "'Waiting for the Sunrise', cursive"
+                        color="rgba(42,238,234)"
+                        fontSize="1.4em"
+                        marginLeft="-2em"
+                        fontFamily="'Waiting for the Sunrise', cursive"
                     >
                         &lt;html&gt;
                     </Text>
                     <Text
-                        color      = "rgba(42,238,234)"
-                        fontSize   = "1.4em"
-                        fontFamily = "'Waiting for the Sunrise', cursive"
+                        color="rgba(42,238,234)"
+                        fontSize="1.4em"
+                        fontFamily="'Waiting for the Sunrise', cursive"
                     >
                         &lt;body&gt;
                     </Text>
                     <Box
-                        marginTop = "10%" 
-                        marginBottom= "1em"
+                        marginTop="10%"
+                        marginBottom="1em"
                     >
                         <Text
-                            color      = "rgba(42,238,234)"
-                            fontSize   = "1.4em"
-                            fontFamily = "'Waiting for the Sunrise', cursive"
+                            color="rgba(42,238,234)"
+                            fontSize="1.4em"
+                            fontFamily="'Waiting for the Sunrise', cursive"
                         >
                             &lt;h1&gt;
                         </Text>
-                        <Heading 
-                            as        = "h1"
-                            size      = "4xl"
-                            display   = "inline-block"
-                            fontSize  = "8xl"
+                        <Heading
+                            as="h1"
+                            size="4xl"
+                            display="inline-block"
+                            fontSize="8xl"
                         >
-                            Hi, 
+                            Hi,
                             <br />
-                            I'm Pranav,
-                            <br /> 
+                            I&apos;m Pranav,
+                            <br />
                             Full Stack Developer.
                         </Heading>
                         <Text
-                            color      = "rgba(42,238,234)"
-                            display    = "inline-block"
-                            fontSize   = "1.4em"
-                            marginLeft = "4%"
-                            fontFamily = "'Waiting for the Sunrise', cursive"
+                            color="rgba(42,238,234)"
+                            display="inline-block"
+                            fontSize="1.4em"
+                            marginLeft="4%"
+                            fontFamily="'Waiting for the Sunrise', cursive"
                         >
                             &lt;/h1&gt;
                         </Text>
                     </Box>
                     <Text
-                        color      = "rgba(42,238,234)"
-                        fontSize   = "1.4em"
-                        fontFamily = "'Waiting for the Sunrise', cursive"
+                        color="rgba(42,238,234)"
+                        fontSize="1.4em"
+                        fontFamily="'Waiting for the Sunrise', cursive"
                     >
                         &lt;p&gt;
                     </Text>
                     <Box>
                         <Text
-                            color      = "gray"
+                            color="gray"
                             // fontWeight = "bold"
-                            fontSize   = '2xl'
+                            fontSize='2xl'
                         >
-                            Designing Web Applications using MERN Stack. 
+                            Designing Web Applications using MERN Stack.
                         </Text>
                     </Box>
                     <Text
-                        color      = "rgba(42,238,234)"
-                        fontSize   = "1.4em"
-                        fontFamily = "'Waiting for the Sunrise', cursive"
+                        color="rgba(42,238,234)"
+                        fontSize="1.4em"
+                        fontFamily="'Waiting for the Sunrise', cursive"
                     >
                         &lt;p&gt;
                     </Text>
                     <Button
-                        color        = "#8fc866"
-                        width        = "xs"
-                        variant      = "outline"
-                        marginTop    = "1em"
-                        fontWeight   = "500"
-                        borderColor  = "#8fc866"
-                        transition   = "0.25s ease-in-out"
-                        borderRadius = "none"
-                        _hover       = {{
+                        color="#8fc866"
+                        width="xs"
+                        variant="outline"
+                        marginTop="1em"
+                        fontWeight="500"
+                        borderColor="#8fc866"
+                        transition="0.25s ease-in-out"
+                        borderRadius="none"
+                        _hover={{
                             boxShadow: "inset -10em 0 0 0 #66c887, inset 10em 0 0 0 #66c887",
                             borderColor: "#66c887",
                             color: "white"
                         }}
                     >
-                        Contact Me 
+                        Contact Me
                     </Button>
                     <Box
-                        marginTop = "5%"
+                        marginTop="5%"
                     >
                         <Text
-                            color      = "rgba(42,238,234)"
-                            fontSize   = "1.4em"
-                            fontFamily = "'Waiting for the Sunrise', cursive"
+                            color="rgba(42,238,234)"
+                            fontSize="1.4em"
+                            fontFamily="'Waiting for the Sunrise', cursive"
                         >
                             &lt;/body&gt;
                         </Text>
                         <Text
-                            color      = "rgba(42,238,234)"
-                            fontSize   = "1.4em"
-                            marginLeft = "-2em"
-                            fontFamily = "'Waiting for the Sunrise', cursive"
+                            color="rgba(42,238,234)"
+                            fontSize="1.4em"
+                            marginLeft="-2em"
+                            fontFamily="'Waiting for the Sunrise', cursive"
                         >
                             &lt;/html&gt;
                         </Text>
