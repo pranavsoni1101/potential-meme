6e93259920bc35c8b73f276f2b63b928e07b1c35
Pranav Soni <pranavsoni1101@gmail.com>
2022-04-09 12:23:42 +0530

    Added about page

---
+++ pages/about.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Section from '../components/Section';
+import SectionContent from '../components/Section/SectionContent';
+
+const About = () => {
+    return(
+        <>
+            <Section>
+                <SectionContent>
+                    This is my about page
+                </SectionContent>
+            </Section>
+        </>
+    )
+}
+
+export default About;
