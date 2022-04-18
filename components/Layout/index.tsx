b2146c59847b85d3ba931533cfad82476343a6cf
Pranav Soni <pranavsoni1101@gmail.com>
2022-04-18 13:51:26 +0530

    Added animations on to page content

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from 'react';
+import { motion } from 'framer-motion';
 import SideNavbar from './SideNavbar';
 
 interface LayoutProps {
@@ -6,10 +7,25 @@ interface LayoutProps {
 }
 
 const Layout = ({children}: LayoutProps) =>{
+
+    const variants = {
+        hidden : { opacity: 0 },
+        enter  : { opacity: 1},
+        exit   : { opacity: 0},
+    } 
+
     return(
         <>
             <SideNavbar />
-            {children}
+            <motion.div
+                exit       = "exit"
+                animate    = "enter"
+                initial    = "hidden"
+                variants   = {variants}
+                transition = {{type: "linear"}}
+            >
+                {children}
+            </motion.div>
         </>
     )
 }
--- components/Section/SectionContent.tsx
+++ components/Section/SectionContent.tsx
@@ -1,54 +1,70 @@
 import React, { ReactNode } from 'react';
+import { motion } from 'framer-motion';
 import { Box, Text } from '@chakra-ui/react';
 
 interface SectionContentProps {
     children: ReactNode | ReactNode[]
 }
 
-const SectionContent = ({ children }: SectionContentProps) => (
-  <>
-    <Box width="100%">
-      <Box
-        marginBottom= "4%"
-      >
-        <Text
-            color      = "rgba(42,238,234)"
-            fontSize   = "1.4em"
-            marginLeft = "-2em"
-            fontFamily = "'Waiting for the Sunrise', cursive"
-        >
-            &lt;html&gt;
-        </Text>
-        <Text
-            color      = "rgba(42,238,234)"
-            fontSize   = "1.4em"
-            fontFamily = "'Waiting for the Sunrise', cursive"
-        >
-            &lt;body&gt;
-        </Text>
-      </Box>
-      {children}
-      <Box
-          marginTop="4%"
-      >
-          <Text
-              color="rgba(42,238,234)"
-              fontSize="1.4em"
-              fontFamily="'Waiting for the Sunrise', cursive"
-          >
-              &lt;/body&gt;
-          </Text>
-          <Text
-              color="rgba(42,238,234)"
-              fontSize="1.4em"
-              marginLeft="-2em"
-              fontFamily="'Waiting for the Sunrise', cursive"
-          >
-              &lt;/html&gt;
-          </Text>
-      </Box>
-    </Box>
-  </>
-);
+const SectionContent = ({ children }: SectionContentProps) => {
+    const variants = {
+        hidden : { opacity: 0, x: -200, y: 0 },
+        enter  : { opacity: 1, x: 0, y: 0 },
+        exit   : { opacity: 0, x: 0, y: -100 },
+    } 
+    return(
+        <>
+            <Box width="100%">
+            <Box
+                marginBottom= "4%"
+            >
+                <Text
+                    color      = "rgba(42,238,234)"
+                    fontSize   = "1.4em"
+                    marginLeft = "-2em"
+                    fontFamily = "'Waiting for the Sunrise', cursive"
+                >
+                    &lt;html&gt;
+                </Text>
+                <Text
+                    color      = "rgba(42,238,234)"
+                    fontSize   = "1.4em"
+                    fontFamily = "'Waiting for the Sunrise', cursive"
+                >
+                    &lt;body&gt;
+                </Text>
+            </Box>
+            <motion.div
+                exit       = "exit"
+                animate    = "enter"
+                initial    = "hidden"
+                variants   = {variants}
+                transition = {{type: "linear", delay: 0.2}}    
+            >
+                {children}
+            </motion.div>
+            <Box
+                marginTop="4%"
+            >
+                <Text
+                    color="rgba(42,238,234)"
+                    fontSize="1.4em"
+                    fontFamily="'Waiting for the Sunrise', cursive"
+                >
+                    &lt;/body&gt;
+                </Text>
+                <Text
+                    color="rgba(42,238,234)"
+                    fontSize="1.4em"
+                    marginLeft="-2em"
+                    fontFamily="'Waiting for the Sunrise', cursive"
+                >
+                    &lt;/html&gt;
+                </Text>
+            </Box>
+            </Box>
+        </>
+        );
+}
 
 export default SectionContent;
